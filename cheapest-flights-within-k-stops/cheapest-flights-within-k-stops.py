626d41281640f69c403896778dad54c21eb3f1a4
홍승용 <jesse1231@naver.com>
2021-12-15 02:08:59 +0900

    Time: 344 ms (17.25%), Space: 15.3 MB (68.27%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -3,21 +3,30 @@ import heapq
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = collections.defaultdict(dict)
-        counts = collections.defaultdict(int)
-        hq = [(0, src, 0)]
-        for f, t, p in flights:
-            graph[f][t] = p
-        for i in range(n):
-            counts[i] = k + 1
-        while len(hq) > 0 :
-            price, city, count = heapq.heappop(hq)
-            if city == dst:
-
-                return price
-            if counts[city] >= count and count + 1 <= k + 1:
-                counts[city] = count
-                for t, p in graph[city].items():
-                    heapq.heappush(hq, (price + p, t, count + 1))
-
-        return -1
+#         flights_dict = collections.defaultdict(list)
+#         for fr, to , pr in flights:
+#             flights_dict[fr].append([pr, to])
+#         hq = [[0, src, 0]]
+#         while len(hq) > 0:
+#             pr, to, nof = heapq.heappop(hq)
+#             if to == dst:
+                    
+#                 return pr
+#             if nof < k + 1:
+#                 for npr, nto in flights_dict[to]:
+#                     heapq.heappush(hq, [pr + npr, nto, nof + 1])
+                    
+#         return -1
+        prev = [float('inf') for _ in range(n)]
+        curr = [float('inf') for _ in range(n)]
+        prev[src] = 0
+        curr[src] = 0
+        count = 0
+        
+        while count < k+1:
+            for f in flights:
+                curr[f[1]] = min(prev[f[0]]+f[2], prev[f[1]], curr[f[1]])
+            prev, curr = curr, prev
+            count += 1
+        
+        return prev[dst] if prev[dst] < float('inf') else -1
