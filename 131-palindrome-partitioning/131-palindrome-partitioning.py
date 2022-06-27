1ed491aacf72871c54008e49e3e02f78155ec8bd
홍승용 <jesse1231@naver.com>
2022-06-27 22:37:57 +0900

    Time: 1209 ms (15.67%), Space: 29.2 MB (73.54%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,7 +1,6 @@
 class Solution:
     result = []
     def partition(self, s: str) -> List[List[str]]:
-        # print(s)
         if len(s) < 1:
 
             return []
@@ -11,11 +10,9 @@ class Solution:
             leftS = s[:i + 1]
             rightS = s[i + 1:]
             if leftS == leftS[::-1]:
-                # print('?', s, leftS)
                 for foundS in [foundS for foundS in self.partition(rightS) if len(foundS) > 0]:
                     result.append([leftS, *foundS])
         if s == s[::-1]:
             result.append([s])
         
-        print(s, result)
         return result
