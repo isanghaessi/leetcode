5cb82942fc9a1398042143a8e29ca4bce4c8befa
홍승용 <jesse1231@naver.com>
2022-07-05 00:49:26 +0900

    Time: 62 ms (36.05%), Space: 13.8 MB (97.32%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        alphaLogs = []
+        numberLogs = []
+        for log in logs:
+            if log.split(' ')[1].isalpha():
+                alphaLogs.append(log)
+            else:
+                numberLogs.append(log)
+        alphaLogs.sort(key = lambda a:(' '.join(a.split(' ')[1:]), a.split(' ')[0]))
+        
+        return alphaLogs + numberLogs
