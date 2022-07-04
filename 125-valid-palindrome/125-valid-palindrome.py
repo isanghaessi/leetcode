9c180231efd7dca7f0a55d67a5b04b3d21ca3fda
홍승용 <jesse1231@naver.com>
2022-07-05 00:25:35 +0900

    Time: 84 ms (34.66%), Space: 14.4 MB (85.76%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         newS = ''
         for _s in s:
-            if _s.isalpha() or _s.isdigit():
+            if _s.isalnum():
                 newS = newS + _s.lower()
         
         return newS == newS[::-1]
