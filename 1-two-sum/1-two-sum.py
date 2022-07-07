da8fb7cfc534e9bad18ba36b155c5b79a640b408
홍승용 <jesse1231@naver.com>
2022-07-07 21:49:07 +0900

    Time: 116 ms (46.75%), Space: 15.2 MB (24.34%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,8 @@
-from collections import *
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsDict = defaultdict(list)
+        numsDict = {}
         for i, num in enumerate(nums):
-            numsDict[num].append(i)
-        for num in list(numsDict.keys()):
-            leftI = numsDict[num].pop()
-            if len(numsDict[target - num]) > 0:
+            if target - num in numsDict:
                 
-                return (leftI, numsDict[target - num].pop())
+                return (i, numsDict[target - num])
+            numsDict[num] = i
