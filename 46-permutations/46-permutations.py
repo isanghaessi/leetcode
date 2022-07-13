4ffd7992eaca48e74f721921fba113093ccb62a4
홍승용 <jesse1231@naver.com>
2022-07-14 00:05:17 +0900

    Time: 85 ms (10.03%), Space: 14.1 MB (21.84%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -2,5 +2,16 @@ from collections import *
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(numsPath):
+            if len(numsPath) >= len(nums):
+                
+                answer.append(numsPath)
+            
+            for num in [num for num in nums if num not in numsPath]:
+                dfs([*numsPath, num])
         
-        return permutations(nums, len(nums))
+        answer = []
+        for num in nums:
+            dfs([num])
+        
+        return answer
