3cc9ffe284ca2ca5000c5f61bb4348e9192bef1c
홍승용 <jesse1231@naver.com>
2022-07-16 15:20:24 +0900

    Time: 1095 ms (6.06%), Space: 15.8 MB (84.87%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,6 +1,15 @@
-from collections import *
-
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        def dfs(nums, numsPath):
+            if len(numsPath) == k:
+                answer.append(numsPath)
+                
+                return
+            for i, num in enumerate(nums):
+                dfs(nums[i + 1:], (*numsPath, num))
         
-        return combinations([i for i in range(1, n + 1)], k)
+        
+        answer = []
+        dfs([i for i in range(1, n + 1)], ())
+        
+        return answer
