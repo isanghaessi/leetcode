5925f24a0eaf1bf34986aec84c0a9f0244b0a079
홍승용 <jesse1231@naver.com>
2022-07-16 16:12:50 +0900

    Time: 53 ms (49.56%), Space: 13.9 MB (99.62%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,16 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def dfs(nums, numsPath, size):
-            if (len(numsPath) == size):
-                answer.append(numsPath)
+        def dfs(nums, numsPath):
+            answer.append(numsPath)
+            
+            if (len(nums) < 1):
                 
                 return
+            
             for i, num in enumerate(nums):
-                dfs(nums[i + 1:], (*numsPath, num), size)
+                dfs(nums[i + 1:], (*numsPath, num))
         
         
-        answer =[]
-        for i in range(len(nums) + 1):
-            dfs(tuple(nums), (), i)
+        answer = []
+        dfs(tuple(nums), ())
         
         return answer
