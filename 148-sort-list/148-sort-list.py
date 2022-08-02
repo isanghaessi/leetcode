e550f8dd805c52f69cbc0030c0e5588dee0dd961
홍승용 <jesse1231@naver.com>
2022-08-02 23:57:37 +0900

    Time: 2244 ms (5.02%), Space: 36.4 MB (89.93%) - LeetHub

---
+++ 148-sort-list/148-sort-list.py
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def merge(head):
+            result = ListNode()
+            result.next = head
+            count = 0
+            slow = fast = head
+            previousSlow = None
+            while fast:
+                if fast.next:
+                    previousSlow = slow
+                    slow = slow.next
+                    fast = fast.next.next
+                    count += 2
+                else:
+                    break
+            if count == 0:
+                
+                return head
+            else:
+                previousSlow.next = None
+                
+                return _merge(head, slow)
+            
+            
+        def _merge(node1, node2):
+            node1 = merge(node1)
+            node2 = merge(node2)
+            result = node =  ListNode()
+            while node1 and node2:
+                if node1.val < node2.val:
+                    node.next = ListNode(node1.val)
+                    node1 = node1.next
+                else:
+                    node.next = ListNode(node2.val)
+                    node2 = node2.next
+                node = node.next
+            while node1:
+                node.next = ListNode(node1.val)
+                node1 = node1.next
+                node = node.next
+            while node2:
+                node.next = ListNode(node2.val)
+                node2 = node2.next
+                node = node.next
+            
+            return result.next
+            
+            
+        return merge(head)
