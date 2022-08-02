78252e0269dd9f31889bfec8e063967a4443e847
홍승용 <jesse1231@naver.com>
2022-08-03 00:03:15 +0900

    Time: 3460 ms (5.02%), Space: 44.2 MB (16.55%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -5,29 +5,6 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def merge(head):
-            result = ListNode()
-            result.next = head
-            count = 0
-            slow = fast = head
-            previousSlow = None
-            while fast:
-                if fast.next:
-                    previousSlow = slow
-                    slow = slow.next
-                    fast = fast.next.next
-                    count += 2
-                else:
-                    break
-            if count == 0:
-                
-                return head
-            else:
-                previousSlow.next = None
-                
-                return _merge(merge(head), merge(slow))
-            
-            
         def _merge(node1, node2):
             result = node =  ListNode()
             while node1 and node2:
@@ -48,6 +25,23 @@ class Solution:
                 node = node.next
             
             return result.next
-            
-            
-        return merge(head)
+        
+        
+        count = 0
+        slow = fast = head
+        previousSlow = None
+        while fast:
+            if fast.next:
+                previousSlow = slow
+                slow = slow.next
+                fast = fast.next.next
+                count += 2
+            else:
+                break
+        if count == 0:
+
+            return head
+        else:
+            previousSlow.next = None
+
+            return _merge(self.sortList(head), self.sortList(slow))
