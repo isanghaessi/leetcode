e35a39ab6d787d02b0f8fdf424c77bd2e6706f7e
홍승용 <jesse1231@naver.com>
2022-08-05 00:29:35 +0900

    Time: 1298 ms (43.80%), Space: 20.2 MB (80.23%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -0,0 +1,6 @@
+from heapq import *
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        return [mc[1] for mc in nsmallest(k, [(abs(x * x + y * y), (x, y)) for x, y in points])]
