3f315dda72a61e5f4797c0a06253d00db11822c5
홍승용 <jesse1231@naver.com>
2022-08-04 23:44:49 +0900

    Time: 46 ms (67.88%), Space: 13.8 MB (63.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,4 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        zeros = []
+        ones = []
+        twos = []
+        for num in nums:
+            if num == 0:
+                zeros.append(num)
+            elif num == 1:
+                ones.append(num)
+            else:
+                twos.append(num)
+        numsLen = len(nums)
+        for zero in zeros[::-1]:
+            nums.append(zero)
+        for one in ones[::-1]:
+            nums.append(one)
+        for two in twos[::-1]:
+            nums.append(two)
+        for _ in range(numsLen):
+            nums.pop(0)
