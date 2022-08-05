654a4f351631efce6addaa991a40ba216ce305e3
홍승용 <jesse1231@naver.com>
2022-08-05 22:52:51 +0900

    Time: 61 ms (77.26%), Space: 14.2 MB (24.85%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,4 +1,31 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        def binSearch(nums, left, right, target):
+            if left > right:
+                
+                return -1
+            
+            index = (left + right) // 2
+            if nums[index] == target:
+                
+                return index
+            elif nums[index] < target:
+                
+                return binSearch(nums, index + 1, right, target)
+            else:
+                
+                return binSearch(nums, left, index - 1, target)
         
-        return list(set(nums1) & set(nums2))
+        nums1 = sorted(list(set(nums1)))
+        nums2 = sorted(list(set(nums2)))
+        nums = nums1 if len(nums1) < len(nums2) else nums2
+        targetNums = nums1 if nums == nums2 else nums2
+        answer = []
+        for num in nums:
+            if num not in answer:
+                index = binSearch(targetNums, 0, len(targetNums) - 1, num)
+                print(num, index)
+                if index >= 0:
+                    answer.append(num)
+        
+        return answer
