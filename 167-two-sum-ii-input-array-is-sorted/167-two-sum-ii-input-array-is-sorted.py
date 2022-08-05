7f2bf8f1e177e82283ebc48e1e7df82facef4589
홍승용 <jesse1231@naver.com>
2022-08-05 23:14:15 +0900

    Time: 296 ms (9.00%), Space: 14.9 MB (41.40%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,12 @@
+from bisect import *
+
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        def binSearch(left, right, target):
-            if left > right:
-                
-                return - 1
-            
-            index = (left + right) // 2
-            if numbers[index] < target:
-                
-                return binSearch(index + 1, right, target)
-            elif numbers[index] > target:
-                
-                return binSearch(left, index - 1, target)
-            else:
-                
-                return index
-        
-        
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:        
         i = 0
         while i < len(numbers):
-            j = binSearch(i + 1, len(numbers) - 1, target - numbers[i])
-            if j >= 0:
+            iTarget = target - numbers[i]
+            j = bisect_left(numbers, iTarget, lo = i + 1)
+            if j < len(numbers) and numbers[j] == iTarget :
                 
                 return (i + 1, j + 1)
             i += 1
