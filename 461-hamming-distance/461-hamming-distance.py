f7b90d93661b90d2ce1871d1692f633a564cfc74
홍승용 <jesse1231@naver.com>
2022-08-06 21:32:10 +0900

    Time: 54 ms (32.53%), Space: 13.8 MB (97.46%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.py
+++ 461-hamming-distance/461-hamming-distance.py
@@ -1,10 +1,4 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        answer = 0
-        xor = x ^ y
-        for i in range(len(max(bin(x), bin(y), key = len)) - 2):
-            pivot = 0b1 << i
-            if xor & pivot:
-                answer += 1
         
-        return answer
+        return bin(x ^ y).count('1')
