6b17cafc6216e6b7ecfa12d80237f23b51076c2b
홍승용 <jesse1231@naver.com>
2022-08-06 21:25:22 +0900

    Time: 151 ms (84.48%), Space: 16.7 MB (50.74%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,10 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        numSet = set()
+        answer = 0
         for num in nums:
-            if num not in numSet:
-                numSet.add(num)
-            else:
-                numSet.remove(num)
-        
-        return list(numSet)[0]
+            answer ^= num
+            
+        return answer
