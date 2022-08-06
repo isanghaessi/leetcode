3cae9eef188c87c9a7d1b223304e68f8bec650c7
홍승용 <jesse1231@naver.com>
2022-08-06 22:28:19 +0900

    Time: 284 ms (9.43%), Space: 14.2 MB (38.38%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -0,0 +1,42 @@
+from collections import *
+
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        def checkPrefix(binStr, prefix):
+            print(binStr, prefix)
+            
+            return binStr[:len(prefix)] == prefix
+        
+        
+        
+        data = deque(data)
+        while len(data) > 0:
+            binStr = bin(data.popleft())[2:]
+            while len(binStr) < 8:
+                binStr = '0' + binStr
+            print('---')
+            print(binStr)
+            if binStr[0] == '1':
+                prefix = ''
+                i = 0
+                while i < len(binStr) and binStr[i] == '1':
+                    prefix += '1'
+                    i += 1
+                prefix += '0'
+                prefixCount = prefix.count('1')
+                if prefixCount < 2 or prefixCount > 4:
+                    
+                    return False
+                for j in range(prefixCount - 1):
+                    if len(data) < 1:
+                        
+                        return False
+                    nextBinStr = bin(data.popleft())[2:]
+                    while len(nextBinStr) < 8:
+                        nextBinStr = '0' + nextBinStr
+                    if not checkPrefix(nextBinStr, '10'):
+                        
+                        return False
+        
+        return True
+                    
