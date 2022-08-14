686c052a4cbfe4ddce9d774efab08e6a97c9b407
홍승용 <jesse1231@naver.com>
2022-08-14 18:21:22 +0900

    Time: 57 ms (21.33%), Space: 13.8 MB (57.10%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    dp = {}
+    
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            
+            return n
+        
+        if n in self.dp:
+            
+            return self.dp[n]
+        
+        self.dp[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
+        return self.dp[n]
