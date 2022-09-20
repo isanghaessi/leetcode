e27ab914281fe88aecd0612e1983a22003b5b2cd
홍승용 <jesse1231@naver.com>
2022-09-20 23:16:09 +0900

    Time: 1138 ms (17.87%), Space: 16.9 MB (11.46%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -3,28 +3,23 @@ from heapq import *
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        def checkAllVisited():
-            for i in range(1, n + 1):
-                if dist[i] == float('inf'):
-
-                    return False
-                
-            return True
-            
-            
         nodeDict = defaultdict(list)
         for fr, to, di in times:
             nodeDict[fr].append((to, di))
         dist = defaultdict(lambda : float('inf'))
         hq = [(0, k)]
-        while len(hq) > 0 and not checkAllVisited():
+        while len(hq) > 0:
             di, current = heappop(hq)
             if dist[current] != float('inf'):
                 
                 continue
                 
-            dist[current] = min(dist[current], di)
+            dist[current] = di
             for to, d in nodeDict[current]:
                 heappush(hq, (dist[current] + d, to))
+        for i in range(1, n + 1):
+            if dist[i] == float('inf'):
+                
+                return - 1
         
-        return max(dist.values()) if checkAllVisited() else -1
+        return max(dist.values())
