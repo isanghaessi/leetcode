18767038be38c551adb85d3fe0fa9ec28703470f
홍승용 <jesse1231@naver.com>
2022-09-20 23:12:41 +0900

    Time: 1329 ms (9.47%), Space: 16.8 MB (11.46%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,22 +1,25 @@
 from collections import *
+from heapq import *
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         nodeDict = defaultdict(list)
-        for f, t, v in times:
-            nodeDict[f].append((v, t))
-        fastTimeDict = {}
-        queue = [(0, k)]
-        while len(queue) > 0:
-            time, node = heappop(queue)
-            if node not in fastTimeDict:
-                fastTimeDict[node] = time
-                for _time, _node in nodeDict[node]:
-                    heappush(queue, (time + _time, _node))
-        
-        for i in range(1, n + 1):
-            if i not in fastTimeDict:
+        for fr, to, di in times:
+            nodeDict[fr].append((to, di))
+        dist = defaultdict(lambda : float('inf'))
+        hq = [(0, k)]
+        while len(hq) > 0:
+            di, current = heappop(hq)
+            if dist[current] != float('inf'):
                 
-                return -1
+                continue
+                
+            dist[current] = min(dist[current], di)
+            for to, d in nodeDict[current]:
+                heappush(hq, (dist[current] + d, to))
+        for i in range(1, n + 1):
+            if dist[i] == float('inf'):
+                
+                return - 1
         
-        return max(fastTimeDict.values())
+        return max(dist.values())
