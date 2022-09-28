de362bfe71220c1d6d42df73987c0efdaddaa7e1
홍승용 <jesse1231@naver.com>
2022-09-29 00:26:38 +0900

    Time: 141 ms (92.56%), Space: 16.7 MB (83.54%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -3,13 +3,7 @@ from collections import *
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         answer = 0
-        dp = {}
         for num in nums:
-            if num in dp:
-                answer -= num
-            else:
-                answer += num
-                dp[num] = True
-        
-        return answer
+            answer ^= num
             
+        return answer
