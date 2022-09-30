bfd99e7f82c7f0d1754c9dc2a0ad00bbe1206f72
홍승용 <jesse1231@naver.com>
2022-09-30 18:17:05 +0900

    Time: 370 ms (24.82%), Space: 15.9 MB (47.54%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -1,14 +1,12 @@
-from heapq import *
+from bisect import *
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
         s.sort()
         answer = 0
-        while len(g) > 0:
-            currentG = g.pop()
-            if len(s) > 0 and currentG <= s[-1]:
-                s.pop()
+        for _s in s:
+            if bisect_right(g, _s) > answer:
                 answer += 1
-        
+                
         return answer
