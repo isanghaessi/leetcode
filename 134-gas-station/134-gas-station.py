e93be96cb0ed2f96a1c8393c0b05d3b84c57db81
홍승용 <jesse1231@naver.com>
2022-09-30 18:01:11 +0900

    Time: 1389 ms (33.95%), Space: 19 MB (74.15%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -2,26 +2,19 @@ from heapq import *
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas) < sum(cost):
+        current = 0
+        for i in range(len(gas)):
+            current += gas[i] - cost[i]
+        if current < 0:
             
             return -1
         
-        calCost = []
-        i = 0
-        while i < len(gas):
-            calCost.append(gas[i] - cost[i])
-            i += 1
-        heap = []
-        for i, cc in enumerate(calCost):
-            heappush(heap, (-cc, i))
-        while len(heap) > 0:
-            _, start = heappop(heap)
-            i = start
-            current = calCost[i]
-            i = (i + 1) % len(calCost)
-            while i != start and current >= 0:
-                current += calCost[i]
-                i = (i + 1) % len(calCost)
-            if i == start and current >= 0:
+        answer = 0
+        current = 0
+        for i in range(len(gas)):
+            current += gas[i] - cost[i]
+            if current < 0:
+                current = 0
+                answer = i + 1
                 
-                return start
+        return answer
