3e40877252f95531826658a3865e2e4c6b34ddc7
홍승용 <jesse1231@naver.com>
2022-09-30 21:53:19 +0900

    Time: 310 ms (44.32%), Space: 15.5 MB (34.08%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,11 +1,4 @@
-from collections import *
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        numDict = defaultdict(int)
-        halfNumsLen = len(nums) // 2
-        for num in nums:
-            numDict[num] += 1
-            if numDict[num] > halfNumsLen:
-                
-                return num
+        
+        return sorted(nums)[len(nums) // 2]
