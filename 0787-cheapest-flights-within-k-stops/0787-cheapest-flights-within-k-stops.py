acc0345cb2c30e56f5a0502c5aee396e5d05cb6c
홍승용 <jesse1231@naver.com>
2022-10-11 23:51:09 +0900

    Time: 213 ms (73.23%), Space: 15.4 MB (36.40%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,42 +1,23 @@
-# from collections import *
-
-# class Solution:
-#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         flightDict = defaultdict(list)
-#         for fr, to, pr in flights:
-#             flightDict[fr].append((pr, to))
-#         hq = [(0, src, -1)]
-#         while len(hq) > 0:
-#             price, current, count = heappop(hq)
-#             if current == dst:
-                
-#                 return price
-            
-#             if count + 1 <= k:
-#                 for pr, to in flightDict[current]:
-#                     heappush(hq, (price + pr, to, count + 1))
-        
-#         return -1
-import collections
-import heapq
+from collections import *
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        flights_dict = collections.defaultdict(list)
-        for fr, to , pr in flights:
-            flights_dict[fr].append([pr, to])
-        prices = collections.defaultdict(lambda :float('inf'))
-        nofs = collections.defaultdict(int)
-        hq = [[0, src, 0]]
+        flightDict = defaultdict(list)
+        for fr, to, pr in flights:
+            flightDict[fr].append((pr, to))
+        hq = [(0, src, -1)]
+        countDict = defaultdict(lambda : float('inf'))
         while len(hq) > 0:
-            pr, to, nof = heapq.heappop(hq)
-            if to == dst:
-                    
-                return pr
-            if (pr < prices[to] or nof < nofs[to]) and nof < k + 1:
-                prices[to] = pr
-                nofs[to] = nof
-                for npr, nto in flights_dict[to]:
-                    heapq.heappush(hq, [pr + npr, nto, nof + 1])
-                    
+            price, current, count = heappop(hq)
+            if count > k or count >= countDict[current]:
+                
+                continue
+            if current == dst:
+                
+                return price
+            
+            countDict[current] = count
+            for pr, to in flightDict[current]:
+                heappush(hq, (price + pr, to, count + 1))
+        
         return -1
