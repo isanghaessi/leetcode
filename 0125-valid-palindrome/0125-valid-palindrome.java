a8b012f1dfecc66a492e334c3f25dccd3908857f
Seungyong Hong <jesse1231@naver.com>
2026-04-13 14:45:24 +0900

    Time: 2 ms (99.37%), Space: 44.3 MB (78.08%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            char ls = s.charAt(l);
+            char rs = s.charAt(r);
+
+            if(!Character.isLetterOrDigit(ls)) {
+                l++;
+            }
+            else if(!Character.isLetterOrDigit(rs)) {
+                r--;
+            } else {
+                if(Character.toLowerCase(ls) != Character.toLowerCase(rs)) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+        }
+
+        return true;
+    }
+}
