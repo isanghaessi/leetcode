76e9b7e9fa6f51b73bd01a1fc785930def501a78
Seungyong Hong <jesse1231@naver.com>
2026-04-14 02:46:37 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -24,7 +24,7 @@ class Solution {
     
         int reverseIndex = remove(current, current.next, n) + 1;
 
-        if (reverseIndex == n && previous != null) {
+        if (reverseIndex == n) {
             previous.next = current.next;
         }
 
