4d7f9162c70a125e39cca8b32a3da0357a6ce9ed
Seungyong Hong <jesse1231@naver.com>
2026-04-15 00:17:27 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,48 @@
+import java.util.*;
+
 class Solution {
     public int numIslands(char[][] grid) {
         int result = 0;
 
+        ArrayDeque<int[]> stack = new ArrayDeque<>();
+
         for (int i = 0; i < grid.length; i++ ) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
-                    continue;
-                }
-
                 if (grid[i][j] == '0') {
                     continue;
                 }
 
                 result++;
-                check(grid, i, j);
+
+                stack.push(new int[] {i, j});
+                while (true) {
+                    int[] current = stack.pop();
+
+                    if (current == null) {
+                        break;
+                    }
+
+                    int _i = current[0];
+                    int _j = current[1];
+
+                    if (_i < 0 || _i >= grid.length || _j < 0 || _j >= grid[0].length) {
+                        break;
+                    }
+
+                    if (grid[_i][_j] == '0') {
+                        break;
+                    }
+
+                    grid[_i][_j] = '0';
+
+                    stack.push(new int[] {_i - 1, _j});
+                    stack.push(new int[] {_i, _j - 1});
+                    stack.push(new int[] {_i + 1, _j});
+                    stack.push(new int[] {_i, _j + 1});
+                }
             }
         }
 
         return result;
-    }
-
-    void check(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
-            return;
-        }
-
-        if (grid[i][j] == '0') {
-            return;
-        }
-
-        grid[i][j] = '0';
-
-        check(grid, i - 1, j);
-        check(grid, i, j - 1);
-        check(grid, i + 1, j);
-        check(grid, i, j + 1);
     }
 }
