6eb849cb955e17369d6ea6258228e89017b5292c
Seungyong Hong <jesse1231@naver.com>
2026-04-16 01:32:51 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,31 +1,29 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Map<Integer, Integer> dp = new HashMap<>();
-        dp.put(0, 0);
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
 
         for (int i = 1; i <= amount; i++) {
-            int current = -1;
-
             for (int j = 0; j < coins.length; j++) {
                 if (i - coins[j] < 0) {
                     continue;
                 }
 
-                int previous = dp.getOrDefault(i - coins[j], -1);
-                if (previous < 0) {
+                int previousIndex = i - coins[j];
+                if (previousIndex < 0 || dp[previousIndex] < 0) {
                     continue;
                 }
 
-                if (current < 0) {
-                    current = previous + 1;
+                int currentCount = dp[previousIndex] + 1;
+                if (dp[i] < 0) {
+                    dp[i] = currentCount;
                 } else {
-                    current = Math.min(current, previous + 1);
+                    dp[i] = Math.min(dp[i], currentCount);
                 }
             }
-
-            dp.put(i, current);
         }
 
-        return dp.get(amount);
+        return dp[amount];
     }
 }
