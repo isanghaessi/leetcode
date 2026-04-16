2c78c87238d40e758097db65809f97d80bc73210
Seungyong Hong <jesse1231@naver.com>
2026-04-16 23:09:40 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -7,44 +7,32 @@ class Solution {
         int jMin = 0;
         int jMax = matrix[0].length - 1;
         
-        int i = iMin;
-        int j = jMin;
-        addAndMark(matrix, i, j, result);
         while (iMin <= iMax && jMin <= jMax) {
-            while (j + 1 <= jMax) {
-                j++;
-                addAndMark(matrix, i, j, result);
+            for (int j = jMin; j <= jMax; j++) {
+                result.add(matrix[iMin][j]);
             }
             iMin++;
 
-            while (i + 1 <= iMax) {
-                i++;
-                addAndMark(matrix, i, j, result);
+            for (int i = iMin; i <= iMax; i++) {
+                result.add(matrix[i][jMax]);
             }
             jMax--;
 
-            while (j - 1 >= jMin) {
-                j--;
-                addAndMark(matrix, i, j, result);
+            if (iMin < iMax) {
+                for (int j = jMax; j >= jMin; j--) {
+                    result.add(matrix[iMax][j]);
+                }
             }
             iMax--;
 
-            while (i - 1 >= iMin) {
-                i--;
-                addAndMark(matrix, i, j, result);
+            if (jMin < jMax) {
+                for (int i = iMax; i >= iMin; i--) {
+                    result.add(matrix[i][jMin]);
+                }
             }
             jMin++;
         }
 
         return result;
-    }
-
-    void addAndMark(int[][] matrix, int i, int j, List<Integer> result) {
-        if (matrix[i][j] > 100) {
-            return;
-        }
-
-        result.add(matrix[i][j]);
-        matrix[i][j] = 101;
     }
 }
