0b34262f55bc745731fa4d3b8a8d17cb6f5911e1
Seungyong Hong <jesse1231@naver.com>
2026-04-16 23:00:37 +0900

    Time: 19 ms (0.91%), Space: 49.6 MB (12.2%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        int iMin = 0;
+        int iMax = matrix.length - 1;
+        int jMin = 0;
+        int jMax = matrix[0].length - 1;
+        
+        int i = iMin;
+        int j = jMin;
+        addAndMark(matrix, i, j, result);
+        while (iMin <= iMax && jMin <= jMax) {
+            while (j + 1 <= jMax) {
+                j++;
+                addAndMark(matrix, i, j, result);
+            }
+            iMin++;
+
+            while (i + 1 <= iMax) {
+                i++;
+                addAndMark(matrix, i, j, result);
+            }
+            jMax--;
+
+            while (j - 1 >= jMin) {
+                j--;
+                addAndMark(matrix, i, j, result);
+            }
+            iMax--;
+
+            while (i - 1 >= iMin) {
+                i--;
+                addAndMark(matrix, i, j, result);
+            }
+            jMin++;
+        }
+
+        return result;
+    }
+
+    void addAndMark(int[][] matrix, int i, int j, List<Integer> result) {
+        System.out.println("" + i+ j+ matrix[i][j]+ result);
+        if (matrix[i][j] > 100) {
+            return;
+        }
+
+        result.add(matrix[i][j]);
+        matrix[i][j] = 101;
+    }
+}
