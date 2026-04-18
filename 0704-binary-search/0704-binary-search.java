31035fec3186a72fea17a2b9cc1d7168486c2fa3
Seungyong Hong <jesse1231@naver.com>
2026-04-18 23:16:40 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,22 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int current = nums[mid];
-        
-            if (current == target) {
-                return mid;
-            }
+        return _search(nums, target, 0);
+    }
 
-            if(current < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    int _search(int[] nums, int target, int offset) {
+        if (nums == null || nums.length < 1) {
+            return -1;
         }
 
-        return -1;
+        int mid = nums.length / 2;
+        int current = nums[mid];
+        
+        if (current == target) {
+            return offset + mid;
+        } else if (current < target) {
+            return _search(Arrays.copyOfRange(nums, mid + 1, nums.length), target, offset + mid + 1);
+        } else {
+            return _search(Arrays.copyOfRange(nums, 0, mid), target, offset);
+        }
     }
 }
