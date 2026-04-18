6f8c5711e5ec8364e96cdc6169bb63ad2c25c676
Seungyong Hong <jesse1231@naver.com>
2026-04-18 20:49:55 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,34 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] > 0) {
-                continue;
-            }
+        int goal = nums.length() - 1;
 
-            boolean check = false;
-
-            int j = i;
-            while (j >= 0) {
-                int minimal = i - j + 1;
-
-                if (j + minimal >= nums.length) {
-                    check = true;
-                    break;
-                }
-
-                if (nums[j] >= minimal) {
-                    check = true;
-                    break;
-                }
-
-                j--;
-            }
-
-            if (!check) {
-                return false;
+        for (int i = goal - 1; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
             }
         }
 
-        return true;
+        return goal == 0;
     }
 }
