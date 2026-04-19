ec8645e0967bb0443e36c6ea76fcac25c4742e79
Seungyong Hong <jesse1231@naver.com>
2026-04-20 03:09:55 +0900

    Time: 30 ms (7.81%), Space: 50.8 MB (6.83%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> timeMap = new HashMap<>();
+        for (int[] time: times) {
+            timeMap.putIfAbsent(time[0], new ArrayList<>());
+            timeMap.get(time[0]).add(new int[] {time[1], time[2]});
+        }
+
+        int[] result = new int[n + 1];
+        Arrays.fill(result, -1);
+        result[0] = 0;
+        result[k] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.offer(new int[] {k, 0});
+        while (pq.size() > 0) {
+            int[] current = pq.poll();
+            int node = current[0];
+            int time = current[1];
+
+            if (result[node] < 0) {
+                result[node] = time;
+            } else {
+                result[node] = Math.min(result[node], time);
+            }
+
+            List<int[]> toGos = timeMap.get(node);
+            if (toGos == null) {
+                continue;
+            }
+
+            for (int[] toGo: toGos) {
+                pq.offer(new int[] {toGo[0], time + toGo[1]});
+            }
+            timeMap.remove(node);
+        }
+
+        int answer = result[0];
+        for(int res: result) {
+            if (res < 0) {
+                return -1;
+            }
+
+            answer = Math.max(answer, res);
+        }
+
+        return answer;
+    }
+}
