31fb64bef5bc88644fa409090727ce5f6b7183d9
Seungyong Hong <jesse1231@naver.com>
2026-04-21 02:13:28 +0900

    Time: 3 ms (13.03%), Space: 46.4 MB (9.47%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (str1.length() < str2.length()) {
+            String temp = str1;
+            str1 = str2;
+            str2 = temp;
+        }
+
+        String result = str2;
+
+        for (int i = str2.length(); i >= 0; i--) {
+            result = str2.substring(0, i);
+
+            if (check(result, str1) && check(result, str2)) {
+                return result;
+            }
+        }
+
+        return result;
+    }
+
+    boolean check(String divide, String str) {
+        if (divide.length() < 1) {
+            return true;
+        }
+
+        if (str.length() % divide.length() > 0) {
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length() / divide.length(); i ++) {
+            sb.append(divide);
+        }
+
+        return sb.toString().equals(str);
+    }
+}
