31491ab1bfc267a74fb4eecc0804fe5e69dd262e
Seungyong Hong <jesse1231@naver.com>
2026-04-20 21:03:37 +0900

    Time: 99 ms (5.03%), Space: 47.4 MB (6.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,47 +1,30 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> flightMap = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            flightMap.putIfAbsent(i, new ArrayList<>());
-        }
-        for (int[] flight: flights) {
-            flightMap.get(flight[0]).add(Arrays.copyOfRange(flight, 1, flight.length));
-        }
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        result[src] = 0;
 
-        int[] minHops = new int[n];
-        Arrays.fill(minHops, -1);
+        for (int i = 0; i < k + 1; i++) {
+            int[] newResult = Arrays.copyOfRange(result, 0, result.length);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.offer(new int[] {src, 0, 1});
+            for (int[] flight: flights) {
+                int from = flight[0];
+                int to = flight[1];
+                int time = flight[2];
 
-        while (pq.size() > 0) {
-            int[] current = pq.poll();
-            int node = current[0];
-            int time = current[1];
-            int hop = current[2];
+                if (result[from] < 0) {
+                    continue;
+                }
 
-            if (hop - 2 > k) {
-                continue;
+                newResult[to] = Arrays.stream(new int[] {result[to], newResult[to], result[from] + time})
+                    .filter((a) -> a >= 0)
+                    .min()
+                    .getAsInt();
             }
 
-            if (node == dst) {
-                return time;
-            }
-
-            if (minHops[node] > 0 && minHops[node] <= hop) {
-                continue;
-            }
-            minHops[node] = hop;
-
-            for(int[] toGo : flightMap.get(node)) {
-                int newNode = toGo[0];
-                int newTime = time + toGo[1];
-                int newHop = hop + 1;
-
-                pq.offer(new int[] {newNode, newTime, newHop});
-            }
+            result = newResult;
         }
 
-        return -1;
+        return result[dst];
     }
 }
