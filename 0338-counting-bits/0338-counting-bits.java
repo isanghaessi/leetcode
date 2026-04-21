3b7e0e253e0d3e4f0c11a6462885dcaf27ecf7d4
Seungyong Hong <jesse1231@naver.com>
2026-04-22 02:09:46 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,22 +1,32 @@
 class Solution {
     public int[] countBits(int n) {
         int[] result = new int[n + 1];
+
+        List<Integer> bin = new ArrayList<>();
+        bin.add(0);
         
-        for (int i = 0; i <= n; i++) {
-            result[i] = get(i);
+        for (int i = 1; i <= n; i++) {
+            result[i] = plus(bin);
         }
 
         return result;
     }
 
-    int get(int n) {
-        int result = 0;
+    int plus(List<Integer> bin) {
+        int i = 0;
+        bin.set(i, bin.get(i) + 1);
 
-        while (n > 0) {
-            result += (n % 2);
-            n /= 2;
+        while (bin.get(i) > 1) {
+            bin.set(i, 0);
+
+            i++;
+
+            if (i >= bin.size()) {
+                bin.add(0);
+            }
+            bin.set(i, bin.get(i) + 1);
         }
 
-        return result;
+        return bin.stream().filter((v) -> v == 1).toList().size();
     }
 }
