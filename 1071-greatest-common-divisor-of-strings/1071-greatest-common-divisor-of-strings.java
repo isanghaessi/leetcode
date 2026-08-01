42006e1bf0c62f8792737d3e82dbda8bc505515b
Seungyong Hong <jesse1231@naver.com>
2026-08-01 23:38:01 +0900

    Time: 1 ms (94.64%), Space: 43.8 MB (15.82%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,40 +1,17 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        int len = Math.min(str1.length(), str2.length());
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            if (str1.charAt(i) != str2.charAt(i)) {
-                break;
-            }
-
-            sb.append(str1.charAt(i));
-        }
-        String candidate = sb.toString();
-
-        for (int i = candidate.length(); i >= 0; i--) {
-            String current = candidate.substring(0, i);
-            if (doDivide(str2, current) && doDivide(str1, current)) {
-                return current;
-            }
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
         }
 
-        return "";
+        return str1.substring(0, gcd(str1.length(), str2.length()));
     }
 
-    private boolean doDivide(String s, String t) {
-        if (t.length() == 0) {
-            return false;
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
         }
-        if (s.length() % t.length() > 0) {
-            return false;
-        }
-
-        StringBuilder sb = new StringBuilder(t);
-        while (sb.length() < s.length()) {
-            sb.append(t);
-        }
-
-        return s.equals(sb.toString());
     }
 }
