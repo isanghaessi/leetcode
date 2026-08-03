31bd29112d9993008dca71b7e141b2e39374cf83
Seungyong Hong <jesse1231@naver.com>
2026-08-03 23:32:42 +0900

    Time: 4 ms (6.09%), Space: 65.2 MB (66.62%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] forward = new int[nums.length];
+        Arrays.fill(forward, 1);
+        for (int i = 0; i < forward.length - 1; i++) {
+            forward[i + 1] = forward[i] * nums[i];
+        }
+
+        int[] reverse = new int[nums.length];
+        Arrays.fill(reverse, 1);
+        for (int i = reverse.length - 1; i >= 1; i--) {
+            reverse[i - 1] = reverse[i] *  nums[i];
+        }
+
+        int[] result = new int[nums.length];
+        Arrays.fill(result, 1);
+        for (int i = 0; i < forward.length; i++) {
+            result[i] *= forward[i] * reverse[i];
+        }
+
+        return result;
+    }
+}
