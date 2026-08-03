1b738c851b10bf7ce8f8054361c72127ec3d8288
Seungyong Hong <jesse1231@naver.com>
2026-08-04 01:21:42 +0900

    Time: 8 ms (6.89%), Space: 126.9 MB (5.2%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -3,13 +3,10 @@ import java.util.*;
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         int[] mins = Arrays.copyOfRange(nums, 0, nums.length);
+        int[] maxs = Arrays.copyOfRange(nums, 0, nums.length);
         for (int i = 1; i < nums.length; i++) {
             mins[i] = Math.min(mins[i - 1], mins[i]);
-        }
-
-        int[] maxs = Arrays.copyOfRange(nums, 0, nums.length);
-        for (int i = nums.length - 2; i >= 0; i--) {
-            maxs[i] = Math.max(maxs[i + 1], maxs[i]);
+            maxs[nums.length - 1 - i] = Math.max(maxs[nums.length - i], maxs[nums.length - 1 - i]);
         }
 
         for (int i = 0; i < nums.length; i++) {
