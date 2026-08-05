8e56f85914cf37e2db73bbdfdc48f13e43310c52
Seungyong Hong <jesse1231@naver.com>
2026-08-06 01:26:19 +0900

    Time: 30 ms (6.24%), Space: 47.4 MB (93.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,26 +1,26 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        int count = 0;
-        while (i < nums.length - count) {
-            if (nums[i] == 0) {
-                shift(nums, i);
-                count++;
-            } else {
-                i++;
+        int zi = 0;
+        while (zi < nums.length && nums[zi] != 0) {
+            zi++;
+        }
+
+        while (zi < nums.length) {
+            int ni = zi;
+            while (ni < nums.length && nums[ni] == 0) {
+                ni++;
+            }
+
+            if (ni >= nums.length) {
+                break;
+            }
+
+            nums[zi] = nums[ni];
+            nums[ni] = 0;
+
+            while (zi < nums.length && nums[zi] != 0) {
+                zi++;
             }
         }
-    }
-
-    private void shift(int[] array, int offset) {
-        if (offset + 1 > array.length) {
-            return;
-        }
-
-        for (int i = offset; i < array.length - 1; i++) {
-            array[i] = array[i + 1];
-        }
-
-        array[array.length - 1] = 0;
     }
 }
