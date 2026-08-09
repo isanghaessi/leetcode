4f96aa8032994fea835bbc394f5b0e0cec85e9ba
Seungyong Hong <jesse1231@naver.com>
2026-08-10 01:03:20 +0900

    Time: 119 ms (9.14%), Space: 47.2 MB (89.2%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] left = new int[nums.length + 2];
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] + nums[i - 1];
+        }
+        left[nums.length] = left[nums.length - 1];
+
+        int[] right = new int[nums.length + 2];
+        for (int i = nums.length; i >= 1; i--) {
+            right[i] = right[i + 1] + nums[i - 1];
+        }
+        right[0] = right[1];
+
+        for (int i = 1; i <= nums.length; i++) {
+            System.out.println(left[i - 1] + " " + right[i + 1]);
+            if (left[i - 1] == right[i + 1]) {
+                return i - 1;
+            }
+        }
+
+        return -1;
+    }
+}
