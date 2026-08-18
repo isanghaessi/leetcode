ff666b1756ab88315480a416acf2a4943f81ab8c
Seungyong Hong <jesse1231@naver.com>
2026-08-19 02:19:30 +0900

    Time: 51 ms (17.21%), Space: 58.3 MB (5.06%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,35 +1,29 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         Map<String, Integer> rowCounter = new HashMap<>();
-
-        for (int i = 0; i < grid.length; i++) {
-            StringBuilder sb = new StringBuilder();
-
-            for (int j = 0; j < grid[0].length; j++) {
-                sb.append(String.valueOf(grid[i][j]));
-                sb.append(",");
-            }
-
-            String key = sb.toString();
-            int value = rowCounter.getOrDefault(key, 0) + 1;
-            rowCounter.put(key, value);
-        }
-
         Map<String, Integer> colCounter = new HashMap<>();
 
         for (int i = 0; i < grid.length; i++) {
-            StringBuilder sb = new StringBuilder();
+            StringBuilder rowSb = new StringBuilder();
+            StringBuilder colSb = new StringBuilder();
 
             for (int j = 0; j < grid[0].length; j++) {
-                sb.append(String.valueOf(grid[j][i]));
-                sb.append(",");
+                rowSb.append(String.valueOf(grid[i][j]));
+                rowSb.append(",");
+
+                colSb.append(String.valueOf(grid[j][i]));
+                colSb.append(",");
             }
 
-            String key = sb.toString();
-            int value = colCounter.getOrDefault(key, 0) + 1;
-            colCounter.put(key, value);
-        }
+            String rowKey = rowSb.toString();
+            int rowValue = rowCounter.getOrDefault(rowKey, 0) + 1;
+            rowCounter.put(rowKey, rowValue);
 
+            String colKey = colSb.toString();
+            int colValue = colCounter.getOrDefault(colKey, 0) + 1;
+            colCounter.put(colKey, colValue);
+        }
+        
         int result = 0;
 
         for (String rowKey : rowCounter.keySet()) {
