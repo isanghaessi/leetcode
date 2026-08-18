86207bfaa1e7eb765b0a3bcc2ca99ad84b6cb9ac
Seungyong Hong <jesse1231@naver.com>
2026-08-19 02:23:04 +0900

    Time: 50 ms (17.36%), Space: 56.6 MB (7.17%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,36 +1,41 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         Map<String, Integer> rowCounter = new HashMap<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < grid[0].length; j++) {
+                sb.append(String.valueOf(grid[i][j]));
+                sb.append(",");
+            }
+
+            String key = sb.toString();
+            int value = rowCounter.getOrDefault(key, 0) + 1;
+            rowCounter.put(key, value);
+        }
+
         Map<String, Integer> colCounter = new HashMap<>();
 
         for (int i = 0; i < grid.length; i++) {
-            StringBuilder rowSb = new StringBuilder();
-            StringBuilder colSb = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
 
             for (int j = 0; j < grid[0].length; j++) {
-                rowSb.append(String.valueOf(grid[i][j]));
-                rowSb.append(",");
-
-                colSb.append(String.valueOf(grid[j][i]));
-                colSb.append(",");
+                sb.append(String.valueOf(grid[j][i]));
+                sb.append(",");
             }
 
-            String rowKey = rowSb.toString();
-            int rowValue = rowCounter.getOrDefault(rowKey, 0) + 1;
-            rowCounter.put(rowKey, rowValue);
-
-            String colKey = colSb.toString();
-            int colValue = colCounter.getOrDefault(colKey, 0) + 1;
-            colCounter.put(colKey, colValue);
+            String key = sb.toString();
+            int value = colCounter.getOrDefault(key, 0) + 1;
+            colCounter.put(key, value);
         }
-        
+
         int result = 0;
 
+        Set<String> colKeys = colCounter.keySet();
         for (String rowKey : rowCounter.keySet()) {
-            for (String colKey : colCounter.keySet()) {
-                if (rowKey.equals(colKey)) {
-                    result += rowCounter.get(rowKey) * colCounter.get(colKey);
-                }
+            if (colKeys.contains(rowKey)) {
+                result += rowCounter.get(rowKey) * colCounter.get(rowKey);
             }
         }
 
