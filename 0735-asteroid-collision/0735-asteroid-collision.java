1f4d74ad76471eb3a7536af5cee2ad029be4221a
Seungyong Hong <jesse1231@naver.com>
2026-08-22 19:22:12 +0900

    Time: 2 ms (97.76%), Space: 47 MB (43.07%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,61 +1,29 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        List<Integer> prevResult = collide(Arrays.stream(asteroids).boxed().toList());
-        while (true) {
-            List<Integer> nextResult = collide(prevResult);
+        Deque<Integer> stack = new ArrayDeque<>();
 
-            if (prevResult.equals(nextResult)) {
-                return prevResult.stream().mapToInt(value -> value).toArray();
-            }
-            
-            prevResult = nextResult;
-        }
-    }
-
-    private List<Integer> collide(List<Integer> asteroids) {
-        List<Integer> result = new ArrayList<>();
-
-        int i = 0;
-        while (i < asteroids.size()) {
-            List<Integer> rights = new ArrayList<>();
-            while (i < asteroids.size() && asteroids.get(i) > 0) {
-                rights.add(asteroids.get(i));
-                i++;
-            }
-
-            List<Integer> lefts = new ArrayList<>();
-            while (i < asteroids.size() && asteroids.get(i) < 0) {
-                lefts.add(asteroids.get(i));
-                i++;
-            }
-
-            result.addAll(collide(rights, lefts));
-        }
-
-        return result;
-    }
-
-    private List<Integer> collide(List<Integer> rights, List<Integer> lefts) {
-        int r = rights.size() - 1;
-        int l = 0;
-
-        while (r >= 0 && l < lefts.size()) {
-            if (rights.get(r) < -lefts.get(l)) {
-                r--;
-            } else if (rights.get(r) > -lefts.get(l)) {
-                l++;
+        for (int a : asteroids) {
+            if (a > 0) {
+                stack.push(a);
             } else {
-                r--;
-                l++;
+                while (!stack.isEmpty() && stack.peek() > 0 && Math.abs(stack.peek()) < Math.abs(a)) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(a);
+                }
+
+                if (stack.peek() > 0 && Math.abs(stack.peek()) == Math.abs(a)) {
+                    stack.pop();
+                }
             }
         }
 
-        List<Integer> result = new ArrayList<>();
-        if (r >= 0) {
-            result.addAll(rights.subList(0, r + 1));
-        }
-        if (l < lefts.size()) {
-            result.addAll(lefts.subList(l, lefts.size()));
+        int[] result = new int[stack.size()];
+        int i = stack.size() - 1;
+        while (!stack.isEmpty()) {
+            result[i--] = stack.pop();
         }
 
         return result;
